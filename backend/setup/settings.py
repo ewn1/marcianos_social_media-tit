51ea999480c3bb09840b70f19a9d559ab9499aa9
ewn1 <ewn.francisco@gmail.com>
2026-07-28 18:16:29 -0300

    feat(backend): adiciona configuracao do postgres em settings.py e adiciona variaveis de ambiente

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pvx($8prp@-l*5li#k$%j&3a49gky6$bj&z+km%!r1kgas1@=+"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-pvx($8prp@-l*5li#k$%j&3a49gky6$bj&z+km%!r1kgas1@=+"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -76,12 +82,15 @@ WSGI_APPLICATION = "setup.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# Configurado dinamicamente para o PostgreSQL (NeonDB) via DATABASE_URL do .env
+# Caso não encontre a URL, ele cai de volta no SQLite para fins de teste local rápido se necessário.
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
