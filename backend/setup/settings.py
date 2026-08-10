69284ee109cd81b6f50e77dcbbe9cceb05b9819f
ewn1 <ewn.francisco@gmail.com>
2026-08-10 18:54:35 -0300

    chore: atualiza Dockerfile para gunicorn em produção e prepara infraestrutura de imagenscom cloudinary

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
+    "cloudinary",
     "rest_framework",
     "corsheaders",
     "core",
@@ -141,5 +143,10 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
 }
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
